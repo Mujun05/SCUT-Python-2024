d2617e864a01102c954eb68d09f6e9c0fd12796a
Mujun05 <1447369389@qq.com>
2024-03-13 23:37:46 +0800

    已将实验2-2、2-3修改正确。

--- 习题/习题2-2.py
+++ 习题/习题2-2.py
@@ -2,5 +2,7 @@ while True:
     ThreeDigits=input("请输入一个三位数:")
     if ThreeDigits.isdigit() and len(ThreeDigits)==3:
         print(ThreeDigits[::-1])
+        input("输入任意键并回车退出程序。")
+        break
     else:
         print("您的输入不符合标准")
