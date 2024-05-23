8ae24f6622ff3d724926115b38a4ce8a76f46f7e
Mujun05 <1447369389@qq.com>
2024-05-23 22:19:58 +0800

    添加了适当的注释

--- 实验/实验7-1-1.py
+++ 实验/实验7-1-1.py
@@ -1,8 +1,18 @@
+import sys
+sys.set_int_max_str_digits(0)
+# 变量：答案列表
 ans_ls=[]
+# 变量：阶乘的结果
 product=1
-for i in range(1,101):
+# 构造一个从1到100的循环
+for i in range(1,100001):
+    # 阶乘的结果乘i再赋给阶乘的结果
     product*=i
+    # 按格式将结果加入列表
     ans_ls.append("{}!={}\n".format(i,product))
+# 创建一个文件"阶乘.txt"
 file=open("阶乘.txt","w")
+# 将答案写入文件"阶乘.txt"
 file.writelines(ans_ls)
+# 关闭文件"阶乘.txt"
 file.close()
